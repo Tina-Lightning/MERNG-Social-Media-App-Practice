bc1442d1d35879f57517464cf0152ee5e2f19cf8
Tina-Lightning <c.atherholt@gmail.com>
2020-10-29 11:34:36 -0400

    finishing up the server

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,10 +1,13 @@
+const { AuthenticationError } = require("apollo-server");
+
 const Post = require("../../models/Post");
+const checkAuth = require("../../util/check-auth");
 
 module.exports = {
     Query: {
         async getPosts() {
             try {
-                const posts = await Post.find();
+                const posts = await Post.find().sort({ createdAt: -1 });
                 return posts;
             } catch (err) {
                 throw new Error(err);
@@ -25,7 +28,35 @@ module.exports = {
     },
     Mutation: {
         async createPost(_, { body }, context){
+            const user = checkAuth(context);
+            console.log(user);
 
+            const newPost = new Post({
+                body,
+                user: user.id,
+                username: user.username,
+                createdAt: new Date().toISOString()
+            });
+
+            const post = await newPost.save();
+
+            return post;
+
+        },
+        async deletePost(_, { postID }, context){
+            const user = checkAuth(context);
+
+            try{
+                const post = await Post.findById(postId);
+                if(user.username === post.username){
+                    await post.delete();
+                    return "Post successfully deleted";
+                } else {
+                    throw new AuthenticationError("Action not allowed");
+                }
+            } catch(err) {
+                throw new Error(err);
+            }
         }
     }
 };
