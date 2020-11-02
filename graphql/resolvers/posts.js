241ddc8e879abb76f3b57e2fd1480a038c92045a
Tina-Lightning <c.atherholt@gmail.com>
2020-11-02 08:48:55 -0500

    finishing up the server

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/check-auth");
@@ -27,7 +27,7 @@ module.exports = {
         }
     },
     Mutation: {
-        async createPost(_, { body }, context){
+        async createPost(_, { body }, context) {
             const user = checkAuth(context);
             console.log(user);
 
@@ -40,23 +40,53 @@ module.exports = {
 
             const post = await newPost.save();
 
+            context.pubsub.publish("NEW_POST", {
+                newPost: post
+            });
+
             return post;
 
         },
-        async deletePost(_, { postID }, context){
+        async deletePost(_, { postID }, context) {
             const user = checkAuth(context);
 
-            try{
+            try {
                 const post = await Post.findById(postId);
-                if(user.username === post.username){
+                if (user.username === post.username) {
                     await post.delete();
                     return "Post successfully deleted";
                 } else {
                     throw new AuthenticationError("Action not allowed");
                 }
-            } catch(err) {
+            } catch (err) {
                 throw new Error(err);
             }
+        }, 
+        async likePost(_, { postId }, context) {
+            const { username } = checkAuth(context);
+
+            const post = await Post.findById(postId);
+            if (post) {
+                if (post.likes.find(like => like.username === username)) {
+                    // Post already liked, unlike it
+                    post.likes = post.likes.filter(like => like.username !== username);
+                } else {
+                    // Not liked, like post
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    })
+                }
+                await post.save();
+                return post;
+            } else {
+                throw new UserInputError("Post not found");
+            }
+        }
+    },
+    Subscription: {
+        newPost: {
+            subscribe: (_, __, { pubsub }) => pubsub.asyncIterator("NEW_POST")
         }
     }
 };
